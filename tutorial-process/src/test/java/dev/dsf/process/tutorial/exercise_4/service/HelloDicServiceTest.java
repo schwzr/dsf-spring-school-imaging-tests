172644574457289b899d4d217c14e0cdc0148e3f
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-09 15:53:34 +0100

    Changed namespace from HiGHmed to dev.dsf. New service tasks only need an api instance in their constructor. Replaced the checks for parameter permutations with only one for the api. Updated constants to new version

--- tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_4/service/HelloDicServiceTest.java
+++ tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_4/service/HelloDicServiceTest.java
@@ -1,8 +1,5 @@
 package dev.dsf.process.tutorial.exercise_4.service;
 
-import static dev.dsf.bpe.ConstantsBase.BPMN_EXECUTION_VARIABLE_LEADING_TASK;
-import static dev.dsf.bpe.ConstantsBase.BPMN_EXECUTION_VARIABLE_TARGET;
-import static dev.dsf.bpe.ConstantsBase.NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
@@ -19,10 +16,12 @@ import java.util.Optional;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.variable.value.BooleanValue;
-import dev.dsf.fhir.authorization.read.ReadAccessHelper;
-import dev.dsf.fhir.client.FhirWebserviceClientProvider;
-import dev.dsf.fhir.task.TaskHelper;
-import dev.dsf.fhir.variables.TargetValues.TargetValue;
+
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.service.TaskHelper;
+import dev.dsf.bpe.v1.variables.Variables;
+import dev.dsf.bpe.variables.TargetValues;
+import dev.dsf.bpe.v1.constants.*;
 import dev.dsf.process.tutorial.service.HelloDic;
 import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Task;
@@ -36,17 +35,18 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class HelloDicServiceTest
 {
-	@Mock
-	private FhirWebserviceClientProvider clientProvider;
 
 	@Mock
 	private TaskHelper taskHelper;
 
 	@Mock
-	private ReadAccessHelper readAccessHelper;
+	private DelegateExecution execution;
 
 	@Mock
-	private DelegateExecution execution;
+	private ProcessPluginApi api;
+
+	@Mock
+	private Variables variables;
 
 	private Optional<Constructor<HelloDic>> getConstructor(Class<?>... args)
 	{
@@ -67,25 +67,12 @@ public class HelloDicServiceTest
 	@Test
 	public void testHelloDicConstructorWithAdditionalBooleanParameterExists() throws Exception
 	{
-		// not testing all parameter permutations
-		Optional<Constructor<HelloDic>> constructor = getConstructor(FhirWebserviceClientProvider.class,
-				TaskHelper.class, ReadAccessHelper.class, boolean.class);
-		if (constructor.isEmpty())
-			constructor = getConstructor(boolean.class, FhirWebserviceClientProvider.class, TaskHelper.class,
-					ReadAccessHelper.class);
-		if (constructor.isEmpty())
-			constructor = getConstructor(FhirWebserviceClientProvider.class, boolean.class, TaskHelper.class,
-					ReadAccessHelper.class);
-		if (constructor.isEmpty())
-			constructor = getConstructor(FhirWebserviceClientProvider.class, TaskHelper.class, boolean.class,
-					ReadAccessHelper.class);
+		Optional<Constructor<HelloDic>> constructor = getConstructor(ProcessPluginApi.class);
 
 		if (constructor.isEmpty())
 		{
 			String errorMessage = "One public constructor in class " + HelloDic.class.getSimpleName()
-					+ " with parameters (" + FhirWebserviceClientProvider.class.getSimpleName() + ", "
-					+ TaskHelper.class.getSimpleName() + ", " + ReadAccessHelper.class.getSimpleName()
-					+ ", boolean) expected";
+					+ " with parameters (" + ProcessPluginApi.class.getSimpleName() + ") expected";
 			fail(errorMessage);
 		}
 	}
@@ -120,51 +107,40 @@ public class HelloDicServiceTest
 	@Test
 	public void testHelloDicServiceDoExecute() throws Exception
 	{
-		// not trying all parameter permutations
-		Optional<HelloDic> optService = getInstance(Arrays.asList(FhirWebserviceClientProvider.class, TaskHelper.class,
-				ReadAccessHelper.class, boolean.class), clientProvider, taskHelper, readAccessHelper, true);
-		if (optService.isEmpty())
-			optService = getInstance(Arrays.asList(boolean.class, FhirWebserviceClientProvider.class, TaskHelper.class,
-					ReadAccessHelper.class), true, clientProvider, taskHelper, readAccessHelper);
-		if (optService.isEmpty())
-			optService = getInstance(Arrays.asList(FhirWebserviceClientProvider.class, boolean.class, TaskHelper.class,
-					ReadAccessHelper.class), clientProvider, true, taskHelper, readAccessHelper);
-		if (optService.isEmpty())
-			optService = getInstance(Arrays.asList(FhirWebserviceClientProvider.class, TaskHelper.class, boolean.class,
-					ReadAccessHelper.class), clientProvider, taskHelper, true, readAccessHelper);
+		Optional<HelloDic> optService = getInstance(Arrays.asList(ProcessPluginApi.class));
 
 		assumeTrue(optService.isPresent());
 
 		Task task = getTask();
-		Mockito.when(execution.getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK)).thenReturn(task);
+		Mockito.when(variables.getStartTask()).thenReturn(task);
 		Mockito.when(taskHelper.getFirstInputParameterStringValue(any(),
-				eq("http://highmed.org/fhir/CodeSystem/tutorial"), eq("tutorial-input")))
+				eq("http://dsf.dev/fhir/CodeSystem/tutorial"), eq("tutorial-input")))
 				.thenReturn(Optional.of("Test"));
 
 		optService.get().execute(execution);
 
 		ArgumentCaptor<Task> captor = ArgumentCaptor.forClass(Task.class);
 		Mockito.verify(taskHelper).getFirstInputParameterStringValue(captor.capture(),
-				eq("http://highmed.org/fhir/CodeSystem/tutorial"), eq("tutorial-input"));
-		Mockito.verify(execution, atLeastOnce()).getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK);
+				eq("http://dsf.dev/fhir/CodeSystem/tutorial"), eq("tutorial-input"));
+		Mockito.verify(variables, atLeastOnce()).getStartTask();
 		assertEquals(task, captor.getValue());
 
-		ArgumentCaptor<TargetValue> targetArgumentCaptor = ArgumentCaptor.forClass(TargetValue.class);
-		Mockito.verify(execution).setVariable(eq(BPMN_EXECUTION_VARIABLE_TARGET), targetArgumentCaptor.capture());
+		ArgumentCaptor<TargetValues.TargetValue> targetArgumentCaptor = ArgumentCaptor.forClass(TargetValues.TargetValue.class);
+		Mockito.verify(variables).setVariable(eq(BpmnExecutionVariables.TARGET),targetArgumentCaptor.capture());
 		assertEquals("Test_COS", targetArgumentCaptor.getValue().getValue().getOrganizationIdentifierValue());
 		assertEquals("Test_COS_Endpoint", targetArgumentCaptor.getValue().getValue().getEndpointIdentifierValue());
 		assertEquals("https://cos/fhir", targetArgumentCaptor.getValue().getValue().getEndpointUrl());
 
-		Mockito.verify(execution).setVariable(anyString(), any(BooleanValue.class));
+		Mockito.verify(variables).setVariable(anyString(), any(BooleanValue.class));
 	}
 
 	private Task getTask()
 	{
 		Task task = new Task();
-		task.getRestriction().addRecipient().getIdentifier().setSystem(NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER)
+		task.getRestriction().addRecipient().getIdentifier().setSystem(NamingSystems.OrganizationIdentifier.SID)
 				.setValue("MeDIC");
 		task.addInput().setValue(new StringType("Test")).getType().addCoding()
-				.setSystem("http://highmed.org/fhir/CodeSystem/tutorial").setCode("tutorial-input");
+				.setSystem("http://dsf.dev/fhir/CodeSystem/tutorial").setCode("tutorial-input");
 
 		return task;
 	}
