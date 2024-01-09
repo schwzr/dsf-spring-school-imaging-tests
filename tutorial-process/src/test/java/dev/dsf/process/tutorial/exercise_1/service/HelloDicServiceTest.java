1325809b747a37cfd154fe8c766e9dfc74493d2e
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-09 13:29:20 +0100

    Updated the method of getting the start task (previously "leading task") to the new API version.

--- tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_1/service/HelloDicServiceTest.java
+++ tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_1/service/HelloDicServiceTest.java
@@ -1,8 +1,7 @@
 package dev.dsf.process.tutorial.exercise_1.service;
 
-import static dev.dsf.bpe.ConstantsBase.BPMN_EXECUTION_VARIABLE_LEADING_TASK;
-import static dev.dsf.bpe.ConstantsBase.NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER;
-
+import dev.dsf.bpe.v1.constants.NamingSystems;
+import dev.dsf.bpe.v1.variables.Variables;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import dev.dsf.process.tutorial.service.HelloDic;
 import org.hl7.fhir.r4.model.Task;
@@ -19,23 +18,29 @@ public class HelloDicServiceTest
 	@Mock
 	private DelegateExecution execution;
 
+	@Mock
+	private Variables variables;
+
 	@InjectMocks
 	private HelloDic service;
 
+	//TODO: I'm assuming this test makes sure the user invoked the appropriate method to get access to the recipient organization identifier from the "leading" (now: starting) task. Needs confirmation if the check is still valid the way it is done here
 	@Test
 	public void testHelloDicServiceValid() throws Exception
 	{
-		Mockito.when(execution.getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK)).thenReturn(getTask());
+		//Mockito.when(execution.getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK)).thenReturn(getTask());
+		Mockito.when(variables.getStartTask()).thenReturn(getTask());
 
 		service.execute(execution);
 
-		Mockito.verify(execution).getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK);
+		//Mockito.verify(execution).getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK);
+		Mockito.verify(variables).getStartTask();
 	}
 
 	private Task getTask()
 	{
 		Task task = new Task();
-		task.getRestriction().addRecipient().getIdentifier().setSystem(NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER)
+		task.getRestriction().addRecipient().getIdentifier().setSystem(NamingSystems.OrganizationIdentifier.SID)
 				.setValue("MeDIC");
 
 		return task;
--- tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_2/service/HelloDicServiceTest.java
+++ tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_2/service/HelloDicServiceTest.java
@@ -1,7 +1,5 @@
 package dev.dsf.process.tutorial.exercise_2.service;
 
-import static dev.dsf.bpe.ConstantsBase.BPMN_EXECUTION_VARIABLE_LEADING_TASK;
-import static dev.dsf.bpe.ConstantsBase.NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
@@ -15,11 +13,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.service.FhirWebserviceClientProvider;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+import dev.dsf.bpe.v1.variables.Variables;
 import dev.dsf.fhir.authorization.read.ReadAccessHelper;
-import dev.dsf.fhir.client.FhirWebserviceClientProvider;
-import dev.dsf.fhir.task.TaskHelper;
 import dev.dsf.process.tutorial.service.HelloDic;
+import dev.dsf.bpe.v1.service.TaskHelper;
+import dev.dsf.bpe.v1.constants.*;
 import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Task;
 import org.junit.Test;
@@ -43,6 +45,12 @@ public class HelloDicServiceTest
 
 	@Mock
 	private DelegateExecution execution;
+
+	@Mock
+	private ProcessPluginApi api;
+
+	@Mock
+	private Variables variables;
 
 	private Optional<Constructor<HelloDic>> getConstructor(Class<?>... args)
 	{
@@ -132,17 +140,17 @@ public class HelloDicServiceTest
 		assumeTrue(optService.isPresent());
 
 		Task task = getTask();
-		Mockito.when(execution.getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK)).thenReturn(task);
+		Mockito.when(variables.getStartTask()).thenReturn(task);
 		Mockito.when(taskHelper.getFirstInputParameterStringValue(any(),
-				eq("http://highmed.org/fhir/CodeSystem/tutorial"), eq("tutorial-input")))
+				eq("http://dsf.dev/fhir/CodeSystem/tutorial"), eq("tutorial-input")))
 				.thenReturn(Optional.of("Test"));
 
 		optService.get().execute(execution);
 
 		ArgumentCaptor<Task> captor = ArgumentCaptor.forClass(Task.class);
 		Mockito.verify(taskHelper).getFirstInputParameterStringValue(captor.capture(),
-				eq("http://highmed.org/fhir/CodeSystem/tutorial"), eq("tutorial-input"));
-		Mockito.verify(execution, atLeastOnce()).getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK);
+				eq("http://dsf.dev/fhir/CodeSystem/tutorial"), eq("tutorial-input"));
+		Mockito.verify(variables, atLeastOnce()).getStartTask();
 
 		assertEquals(task, captor.getValue());
 	}
@@ -150,10 +158,10 @@ public class HelloDicServiceTest
 	private Task getTask()
 	{
 		Task task = new Task();
-		task.getRestriction().addRecipient().getIdentifier().setSystem(NAMINGSYSTEM_HIGHMED_ORGANIZATION_IDENTIFIER)
+		task.getRestriction().addRecipient().getIdentifier().setSystem(NamingSystems.OrganizationIdentifier.SID)
 				.setValue("MeDIC");
 		task.addInput().setValue(new StringType("Test")).getType().addCoding()
-				.setSystem("http://highmed.org/fhir/CodeSystem/tutorial").setCode("tutorial-input");
+				.setSystem("http://dsf.dev/fhir/CodeSystem/tutorial").setCode("tutorial-input");
 
 		return task;
 	}
