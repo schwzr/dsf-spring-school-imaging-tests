4649586f33f1554368c16d1962902f005089e843
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-09 11:25:11 +0100

    Changed namespace from HiGHmed to dev.dsf. First iteration on trying to replicate old test behaviour in the new DSF-Version. Tests previously relied on the FHIR-Resources provided by ProcessPluginDefinitions. They no longer offer that functionality. The FHIR-Resource-Objects have to be created in the tests themselves.

--- tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_2/TutorialProcessPluginDefinitionTest.java
+++ tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_2/TutorialProcessPluginDefinitionTest.java
@@ -3,8 +3,12 @@ package dev.dsf.process.tutorial.exercise_2;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -12,26 +16,64 @@ import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.Process;
 import org.camunda.bpm.model.bpmn.instance.ServiceTask;
-import dev.dsf.bpe.ProcessPluginDefinition;
-import dev.dsf.fhir.resources.ResourceProvider;
+
+import camundajar.impl.scala.collection.immutable.Stream;
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.ProcessPluginApiImpl;
+import dev.dsf.bpe.v1.ProcessPluginDefinition;
+import dev.dsf.bpe.v1.config.ProxyConfig;
+import dev.dsf.bpe.v1.plugin.ProcessPluginImpl;
+import dev.dsf.bpe.v1.service.EndpointProvider;
+import dev.dsf.bpe.v1.service.FhirWebserviceClientProvider;
+import dev.dsf.bpe.v1.service.MailService;
+import dev.dsf.bpe.v1.service.OrganizationProvider;
+import dev.dsf.bpe.v1.service.QuestionnaireResponseHelper;
+import dev.dsf.bpe.v1.service.TaskHelper;
+import dev.dsf.bpe.variables.ObjectMapperFactory;
+import dev.dsf.fhir.authorization.process.ProcessAuthorizationHelper;
+import dev.dsf.fhir.authorization.read.ReadAccessHelper;
 import dev.dsf.process.tutorial.ConstantsTutorial;
 import dev.dsf.process.tutorial.TutorialProcessPluginDefinition;
 import dev.dsf.process.tutorial.service.HelloDic;
+
+import org.glassfish.jaxb.runtime.v2.runtime.reflect.opt.Const;
 import org.hl7.fhir.r4.model.CodeSystem;
 import org.hl7.fhir.r4.model.MetadataResource;
+import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.junit.Test;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 
 public class TutorialProcessPluginDefinitionTest
 {
+	private ProxyConfig proxyConfig = mock(ProxyConfig.class);
+	private EndpointProvider endpointProvider = mock(EndpointProvider.class);
+	private FhirContext fhirContext = FhirContext.forR4();
+	private FhirWebserviceClientProvider fhirWebserviceClientProvider = mock(FhirWebserviceClientProvider.class);
+	private MailService mailService = mock(MailService.class);
+	private ObjectMapper objectMapper = ObjectMapperFactory.createObjectMapper(fhirContext);
+	private OrganizationProvider organizationProvider = mock(OrganizationProvider.class);
+	private QuestionnaireResponseHelper questionnaireResponseHelper = mock(QuestionnaireResponseHelper.class);
+	private ProcessAuthorizationHelper processAuthorizationHelper = mock(ProcessAuthorizationHelper.class);
+	private ReadAccessHelper readAccessHelper = mock(ReadAccessHelper.class);
+	private TaskHelper taskHelper = mock(TaskHelper.class);
+
+	private ProcessPluginApi processPluginApi = new ProcessPluginApiImpl(proxyConfig, endpointProvider, fhirContext,
+			fhirWebserviceClientProvider, mailService, objectMapper, organizationProvider, processAuthorizationHelper,
+			questionnaireResponseHelper, readAccessHelper, taskHelper);
+	private ConfigurableEnvironment environment = new StandardEnvironment();
+
 	@Test
 	public void testHelloDicBpmnProcessFile() throws Exception
 	{
 		String filename = "bpe/hello-dic.bpmn";
-		String processId = "highmedorg_helloDic";
+		String processId = "dsfdev_helloDic";
 
 		BpmnModelInstance model = Bpmn
 				.readModelFromStream(this.getClass().getClassLoader().getResourceAsStream(filename));
@@ -54,27 +96,51 @@ public class TutorialProcessPluginDefinitionTest
 		String codeSystemCode = "tutorial-input";
 		String valueSetUrl = "http://dsf.dev/fhir/ValueSet/tutorial";
 
+		String codeSystemFile = "fhir/CodeSystems/tutorial.xml";
+		String valueSetFile = "fhir/CodeSystems/tutorial.xml";
+
+
 		ProcessPluginDefinition definition = new TutorialProcessPluginDefinition();
-		ResourceProvider provider = definition.getResourceProvider(FhirContext.forR4(), getClass().getClassLoader(),
-				new StandardEnvironment());
-		assertNotNull(provider);
+		ProcessPluginImpl processPlugin = createPlugin(definition, false);
 
-		List<MetadataResource> helloDic = provider.getResources(
-				ConstantsTutorial.PROCESS_NAME_FULL_HELLO_DIC + "/" + TutorialProcessPluginDefinition.VERSION,
-				s -> ResourceProvider.empty()).collect(Collectors.toList());
+		var fhirResources = processPlugin.getFhirResources();
 
-		String errorCodeSystem = "Process is missing CodeSystem with url '" + codeSystemUrl + "' and concept '"
+		List<Resource> helloDicResources = fhirResources.get(ConstantsTutorial.PROCESS_NAME_HELLO_DIC + "/" + definition.getVersion());
+
+		Map<String, List<String>> helloDic = definition.getFhirResourcesByProcessId();
+
+		int numberEntries = helloDic.size();
+		String errorTooManyEntries = "Too many processes in Map. Got " + numberEntries + " entries. Expected 1.";
+		assertEquals(errorTooManyEntries, 1, numberEntries);
+
+		String firstKey = helloDic.keySet().stream().findFirst().get();
+		String errorFaultyProcessName = "Process name is either wrong or missing. Expected '" + ConstantsTutorial.PROCESS_NAME_FULL_HELLO_DIC + "' but got '" + firstKey + "'";
+		assertEquals(errorFaultyProcessName, ConstantsTutorial.PROCESS_NAME_HELLO_DIC, firstKey);
+
+		String errorCodeSystem = "Process is missing CodeSystem with file name '" + codeSystemFile + "'";
+		assertEquals(errorCodeSystem, 1, helloDic.get(ConstantsTutorial.PROCESS_NAME_HELLO_DIC).stream().filter(r -> codeSystemUrl.equals(r)).count());
+
+		String errorValueSet = "Process is missing ValueSet with file name '" + valueSetFile + "'";
+		assertEquals(errorValueSet, 1, helloDic.get(ConstantsTutorial.PROCESS_NAME_HELLO_DIC).stream().filter(r -> valueSetFile.equals(r)).count());
+
+		errorCodeSystem = "Process is missing CodeSystem with url '" + codeSystemUrl + "' and concept '"
 				+ codeSystemCode + "' with type 'string'";
-		assertEquals(errorCodeSystem, 1, helloDic.stream().filter(r -> r instanceof CodeSystem).map(r -> (CodeSystem) r)
+		assertEquals(errorCodeSystem, 1, helloDicResources.stream().filter(r -> r instanceof CodeSystem).map(r -> (CodeSystem) r)
 				.filter(c -> codeSystemUrl.equals(c.getUrl()))
 				.filter(c -> c.getConcept().stream().anyMatch(con -> codeSystemCode.equals(con.getCode()))).count());
 
-		String errorValueSet = "Process is missing ValueSet with url '" + valueSetUrl + "'";
-		assertEquals(errorValueSet, 1, helloDic.stream().filter(r -> r instanceof ValueSet).map(r -> (ValueSet) r)
+		errorValueSet = "Process is missing ValueSet with url '" + valueSetUrl + "'";
+		assertEquals(errorValueSet, 1, helloDicResources.stream().filter(r -> r instanceof ValueSet).map(r -> (ValueSet) r)
 				.filter(v -> valueSetUrl.equals(v.getUrl()))
 				.filter(v -> v.getCompose().getInclude().stream().anyMatch(i -> codeSystemUrl.equals(i.getSystem())))
 				.count());
 
 		assertEquals(4, helloDic.size());
+	}
+
+	private ProcessPluginImpl createPlugin(ProcessPluginDefinition processPluginDefinition, boolean draft)
+	{
+		return new ProcessPluginImpl(processPluginDefinition, processPluginApi, draft, Path.of("test.jar"),
+				getClass().getClassLoader(), fhirContext, environment);
 	}
 }
