a38634f8cdb883d5adb42fb81c1258d47de1d8b5
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-18 13:40:26 +0100

    Updated solution to DSF 1.4

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/HelloDic.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/HelloDic.java
@@ -1,5 +1,10 @@
 package dev.dsf.process.tutorial.service;
 
+import static dev.dsf.process.tutorial.ConstantsTutorial.CODESYSTEM_TUTORIAL;
+import static dev.dsf.process.tutorial.ConstantsTutorial.CODESYSTEM_TUTORIAL_VALUE_TUTORIAL_INPUT;
+
+import java.util.Optional;
+
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
 import dev.dsf.bpe.v1.variables.Variables;
@@ -24,7 +29,16 @@ public class HelloDic extends AbstractServiceDelegate
 
 	@Override
 	protected void doExecute(DelegateExecution execution, Variables variables) {
-		logger.info("Hello Dic from organization '{}'", variables.getStartTask().getRestriction()
-				.getRecipientFirstRep().getIdentifier().getValue());
+		if (loggingEnabled)
+		{
+			Optional<String> tutorialInputParameter = api.getTaskHelper().getFirstInputParameterStringValue(
+					variables.getStartTask(), CODESYSTEM_TUTORIAL,
+					CODESYSTEM_TUTORIAL_VALUE_TUTORIAL_INPUT);
+
+			logger.info(
+					"Hello Dic from organization '{}' with message '{}'", variables.getStartTask()
+							.getRestriction().getRecipientFirstRep().getIdentifier().getValue(),
+					tutorialInputParameter.orElse("<no message>"));
+		}
 	}
 }
