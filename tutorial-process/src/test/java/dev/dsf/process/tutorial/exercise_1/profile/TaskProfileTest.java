5f390a0567372bc92eba2e46c4ce01ac16990795
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-18 10:29:02 +0100

    validationRule now uses the resource version instead of the full version

--- tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_1/profile/TaskProfileTest.java
+++ tutorial-process/src/test/java/dev/dsf/process/tutorial/exercise_1/profile/TaskProfileTest.java
@@ -3,6 +3,7 @@ package dev.dsf.process.tutorial.exercise_1.profile;
 import static dev.dsf.process.tutorial.ConstantsTutorial.PROFILE_TUTORIAL_TASK_HELLO_DIC;
 import static dev.dsf.process.tutorial.ConstantsTutorial.PROFILE_TUTORIAL_TASK_HELLO_DIC_MESSAGE_NAME;
 import static dev.dsf.process.tutorial.ConstantsTutorial.PROFILE_TUTORIAL_TASK_HELLO_DIC_INSTANTIATES_CANONICAL;
+import static dev.dsf.process.tutorial.ConstantsTutorial.RESOURCE_VERSION;
 import static dev.dsf.process.tutorial.TutorialProcessPluginDefinition.RELEASE_DATE;
 import static dev.dsf.process.tutorial.TutorialProcessPluginDefinition.VERSION;
 import static org.junit.Assert.assertEquals;
@@ -33,7 +34,7 @@ public class TaskProfileTest
 	private static final Logger logger = LoggerFactory.getLogger(TaskProfileTest.class);
 
 	@ClassRule
-	public static final ValidationSupportRule validationRule = new ValidationSupportRule(VERSION, RELEASE_DATE,
+	public static final ValidationSupportRule validationRule = new ValidationSupportRule(RESOURCE_VERSION, RELEASE_DATE,
 			Arrays.asList("dsf-task-base-1.0.0.xml", "task-hello-dic.xml"),
 			Arrays.asList("dsf-read-access-tag-1.0.0.xml", "dsf-bpmn-message-1.0.0.xml"),
 			Arrays.asList("dsf-read-access-tag-1.0.0.xml", "dsf-bpmn-message-1.0.0.xml"));
