f374fb478125b21056484589e13a81421cc0ef81
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-29 09:22:16 +0100

    Re-added cert generation for Webbrowser Test User as that might become a future tutorial exercise

--- test-data-generator/src/main/java/dev/dsf/process/tutorial/tools/generator/TestDataGenerator.java
+++ test-data-generator/src/main/java/dev/dsf/process/tutorial/tools/generator/TestDataGenerator.java
@@ -33,14 +33,14 @@ public class TestDataGenerator
 		Map<String, CertificateFiles> clientCertificateFilesByCommonName = certificateGenerator
 				.getClientCertificateFilesByCommonName();
 
-		/*CertificateFiles webbrowserTestUser = clientCertificateFilesByCommonName.get("Webbrowser Test User");
+		CertificateFiles webbrowserTestUser = clientCertificateFilesByCommonName.get("Webbrowser Test User");
 		Path p12File = certificateGenerator.createP12(webbrowserTestUser);
-		logger.warn(
+		/*logger.warn(
 				"Install client-certificate and CA certificate from \"{}\" into your browsers certificate store to access fhir and bpe servers with your webbrowser",
 				p12File.toAbsolutePath().toString());*/
 
 		CertificateFiles webbrowserDicClient = clientCertificateFilesByCommonName.get("dic-client");
-		Path p12File = certificateGenerator.createP12(webbrowserDicClient);
+		p12File = certificateGenerator.createP12(webbrowserDicClient);
 		logger.warn(
 				"Install DIC client-certificate and CA certificate from \"{}\" into your browsers certificate store to access fhir and bpe servers with your webbrowser",
 				p12File.toAbsolutePath().toString());
