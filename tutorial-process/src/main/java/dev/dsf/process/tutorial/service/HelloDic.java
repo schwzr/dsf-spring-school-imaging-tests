8757e4e36f2876f6b878776535e1b85755310548
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-01-30 09:51:59 +0100

    Optimized imports and format and sort

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/HelloDic.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/HelloDic.java
@@ -1,27 +1,27 @@
 package dev.dsf.process.tutorial.service;
 
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
-import dev.dsf.bpe.v1.variables.Variables;
-import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dev.dsf.fhir.authorization.read.ReadAccessHelper;
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.variables.Variables;
 
 public class HelloDic extends AbstractServiceDelegate
 {
 
 	private static final Logger logger = LoggerFactory.getLogger(HelloDic.class);
+
 	public HelloDic(ProcessPluginApi api)
 	{
 		super(api);
 	}
 
 	@Override
-	protected void doExecute(DelegateExecution execution, Variables variables) {
-		logger.info("Hello Dic from organization '{}'", variables.getStartTask().getRestriction()
-				.getRecipientFirstRep().getIdentifier().getValue());
+	protected void doExecute(DelegateExecution execution, Variables variables)
+	{
+		logger.info("Hello Dic from organization '{}'",
+				variables.getStartTask().getRestriction().getRecipientFirstRep().getIdentifier().getValue());
 	}
 }
--- tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/spring/config/TutorialConfig.java
@@ -1,13 +1,13 @@
 package dev.dsf.process.tutorial.spring.config;
 
-import dev.dsf.bpe.v1.ProcessPluginApi;
-import dev.dsf.process.tutorial.service.HelloDic;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.process.tutorial.service.HelloDic;
 
 @Configuration
 public class TutorialConfig
