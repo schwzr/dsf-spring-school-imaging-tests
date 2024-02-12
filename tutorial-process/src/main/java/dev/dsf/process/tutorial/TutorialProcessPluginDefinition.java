7f66b2ad005dda19c8c10fb7e5d99df4f8d5c71a
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-02-12 11:18:25 +0100

    Added draft Task for web interface

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/TutorialProcessPluginDefinition.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/TutorialProcessPluginDefinition.java
@@ -46,8 +46,9 @@ public class TutorialProcessPluginDefinition implements ProcessPluginDefinition
 
 		String sTaskHelloDic = "fhir/StructureDefinition/task-hello-dic.xml"; // 's' in the beginning of 'sTaskHelloDic'
 																				// stands for StructureDefinition
+		String tTaskHelloDic = "fhir/Task/task-hello-dic.xml";
 
-		return Map.of(ConstantsTutorial.PROCESS_NAME_FULL_HELLO_DIC, List.of(aHelloDic, sTaskHelloDic));
+		return Map.of(ConstantsTutorial.PROCESS_NAME_FULL_HELLO_DIC, List.of(aHelloDic, sTaskHelloDic, tTaskHelloDic));
 	}
 
 	@Override
